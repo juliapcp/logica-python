76f60cbf9b0438c83b32c9d0d2f7c764a8c8fe7e
JuliaPontes <juliapcp2003@gmail.com>
2023-08-30 22:00:49 -0300

    Adicionados comentários na questão 1

--- questao1/Questao1.py
+++ questao1/Questao1.py
@@ -1,18 +1,26 @@
+# Mensagem de boas-vindas
 print("Bem vindo a loja da Julia Pontes Cardoso Pereira")
+
+# Solicitação de entrada do valor unitário e quantidade do produto
 valorUnitario = float(input("Entre com o valor do produto: "))
 quantidade = float(input("Entre com a quantidade do produto: "))
+
+# Cálculo do valor total sem desconto
 valorSemDesconto = valorUnitario * quantidade
 
-if  (quantidade >= 2000):
+# Utilizando estruturas condicionais para aplicar descontos com base na quantidade
+if quantidade >= 2000:
     desconto = valorSemDesconto * 0.15
-elif (quantidade >= 1000):
+elif quantidade >= 1000:
     desconto = valorSemDesconto * 0.10    
-elif (quantidade >= 200):
+elif quantidade >= 200:
     desconto = valorSemDesconto * 0.05
-else :
+else:
     desconto = 0
 
+# Cálculo do valor total com desconto
 valorComDesconto = valorSemDesconto - desconto
 
-print('Valor SEM DESCONTO: R$: ',valorSemDesconto)
-print('Valor COM DESCONTO: R$: ',valorComDesconto)
+# Apresentação dos valores calculados na saída de console
+print('Valor SEM DESCONTO: R$', valorSemDesconto)
+print('Valor COM DESCONTO: R$', valorComDesconto)
