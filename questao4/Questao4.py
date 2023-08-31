28f9960f16cb2d842999a0d15dca94d41616f3fe
JuliaPontes <juliapcp2003@gmail.com>
2023-08-30 21:57:26 -0300

    Adicionados comentários na questão 4

--- questao4/Questao4.py
+++ questao4/Questao4.py
@@ -1,3 +1,4 @@
+# Definindo a função para cadastrar um colaborador, recebendo um ID como parâmetro
 def cadastrar_colaborador(id):
     while True:
         try: 
@@ -5,33 +6,34 @@ def cadastrar_colaborador(id):
             setor = input("Entre com o setor do colaborador: ")
             pagamento = float(input("Entre com o pagamento do colaborador: "))
             colaborador = {"id": id, "nome": nome, "setor": setor, "pagamento": pagamento}
-            lista_colaboradores.append(colaborador)
+            lista_colaboradores.append(colaborador)  # Adicionando o colaborador à lista
             break
         except:
             print("Entrada inválida")
             continue
 
-def consultar_colaborador():  
-    try :
+# Definindo a função para consultar colaboradores
+def consultar_colaborador():
+    try:
         opcao = int(input("Qual opção deseja? \n 1. Consultar Todos \n 2. Consultar por Id \n 3. Consultar por Setor \n 4. Retornar ao menu \n"))
         if opcao not in [1, 2, 3, 4]:
             print("Opção inválida")
             return
-        if opcao == 1: 
+        if opcao == 1:  # Consultar todos os colaboradores
             print(lista_colaboradores)
-        elif opcao == 2:
+        elif opcao == 2:  # Consultar por ID
             print(lista_colaboradores[int(input("Entre com o id do colaborador: ")) - 1])
-        elif opcao == 3:
+        elif opcao == 3:  # Consultar por Setor
             setor = input("Entre com o setor: ").lower()
             for colaborador in lista_colaboradores:
                 if colaborador["setor"].lower() == setor:
                     print(colaborador)
-        elif opcao == 4:
+        elif opcao == 4:  # Retornar ao menu
             return            
     except ValueError:
         print("Opção inválida")
 
-
+# Definindo a função para remover um colaborador
 def remover_colaborador():
     id = int(input("Entre com o id do colaborador: "))
     for colaborador in lista_colaboradores:
@@ -39,24 +41,26 @@ def remover_colaborador():
             lista_colaboradores.remove(colaborador)
             break
 
+# Mensagem de boas-vindas
 print("Bem-vindo ao sistema de gerenciamento de pessoas da Julia Pontes Cardoso Pereira")
-lista_colaboradores = []
-id_global = 0
+lista_colaboradores = []  # Inicializando a lista de colaboradores vazia
+id_global = 0  # Inicializando o ID global
 
+# Loop principal do programa
 while True:
-    try :
+    try:
         opcao = int(input("Qual opção deseja? \n 1. Cadastrar Colaborador \n 2. Consultar Colaborador \n 3. Remover Colaborador \n 4. Encerrar Programa \n"))
         if opcao not in [1, 2, 3, 4]:
             print("Opção inválida")
             continue
-        if opcao == 1:
+        if opcao == 1:  # Cadastrar colaborador
             id_global += 1
             cadastrar_colaborador(id_global)
-        elif opcao == 2:
+        elif opcao == 2:  # Consultar colaborador
             consultar_colaborador()
-        elif opcao == 3:
+        elif opcao == 3:  # Remover colaborador
             remover_colaborador()
-        elif opcao == 4:
+        elif opcao == 4:  # Encerrar programa
             break        
     except ValueError:
         print("Opção inválida")
